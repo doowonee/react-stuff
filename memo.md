c8df41200f2fc5712f717dd5b4f7208b8641af11
the1900 <the1900.com@gmail.com>
2017-06-14 18:30:02 +0900

    initial commit

---
+++ memo.md
@@ -0,0 +1,8 @@
+# 목표 
+
+react toolbox을 통해 블로그 레이아웃을 만든다. 
+
+# webpack, reacct, ES5, 각종 플러그인들 사용법도 익힐수 있다. 
+
+https://scotch.io/tutorials/setup-a-react-environment-using-webpack-and-babel
+위를 참조하여 초기 설정을 했다.
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundled.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
