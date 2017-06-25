d28320c32f3b0bb1eb2a9f0f85ad176db3a3ad65
the1900 <the1900.com@gmail.com>
2017-06-26 00:20:33 +0900

    functional componets 로 변경 및 export 파악 완료

--- src/components/FloatingMenu.jsx
+++ src/components/FloatingMenu.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import { Button } from 'react-toolbox/lib/button';
 
-export default class FloatingMenu extends React.Component {
+/*export default class FloatingMenu extends React.Component {
   render() {
     return (
       <Button label='add' neutral floating />
      )
   }
+}*/
+
+export default function FloatingMenu(props) {
+  return (
+    <Button label='add' floating />
+  )
 }
+
+// 이렇게 따로 노출해도 되고 애초에 함수 만뜰때 노출해도 되고 ㅇㅇ
+// export default FloatingMenu
