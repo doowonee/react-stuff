4dff7d72ac1c72eac626a27fa3a75be0876a3818
the1900 <the1900.com@gmail.com>
2017-07-19 20:26:30 +0900

    와 viewport 태그가 없으면 픽셀크기랑 해상도 장치크기 무시한다ㄷ
    반응형 웹에서 미디어쿼리보다 뷰포트가 엄청 중요하다는걸 알았다.
    
    다음구현할꺼 FAB버튼 more클릿기 상태 변경하여 위로가는 버튼나오게 하고
    나머지 버튼 나타나도록 하며 스크롤업 자바스크립트로 구현하기

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,7 @@ import ViewCards from './ViewCards.jsx'
 import FloatingMenu from './FloatingMenu'
 
 const cardStyle  = {
-    marginLeft: '48px',
-    marginBottom: '48px',
-    width: '400px'
+    maxWidth: '480px'
 };
 
 
@@ -16,12 +14,12 @@ export default class App extends React.Component {
   render() {
     return (
      <Layout>
-        <Panel>
+        <Panel style={{padding: '16px'}}>
           {/* 왜 style은 curly braces가 2중인지 https://stackoverflow.com/questions/38156010/can-i-use-one-curly-brace-instead-of-two-curly-braces-in-react-props */}
           <h1 style={{textAlign: 'center'}}>react 가동완료</h1>
           <ViewCards style={cardStyle} />
-          <FloatingMenu />
         </Panel>
+          <FloatingMenu />
       </Layout> );
   }
 }
