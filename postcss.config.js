34d47e6b11426199a7a9a63ebc52b7242da41912
the1900 <the1900.com@gmail.com>
2017-07-29 20:23:25 +0900

    bugfix FloatMenu button and resize NavDrawer width

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@ const overriddenVariables = {
   'color-text': 'var(--palette-grey-800)',
   'color-primary': 'var(--palette-blue-500)',
   'color-accent': 'var(--palette-teal-600)',
-  'preferred-font' : "'Spoqa Han sans', 'Arial', sans-serif;"
+  'preferred-font' : "'Spoqa Han sans', 'Arial', sans-serif;",
+  'drawer-desktop-width': "calc(5 * var(--standard-increment-desktop));"
 };
 
 module.exports = {
